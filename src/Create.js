17958f0516dc4a0eb69b478171f8a91245c5c70a
Guxim <guxim.selmani1@hotmail.com>
2021-06-30 18:02:24 +0200

    NEW: Create blog!

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+  };
   return (
     <div className="create">
       <h1>Add a New Blog!</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Content:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
